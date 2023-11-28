148a4f8121c54629a5380af5dcf66ebf9b87c4bc
Pritam-00 <pritambera11235@gmail.com>
2023-11-28 19:42:36 +0530

    build done

--- src/components/Header Comp/NavHeader.js
+++ src/components/Header Comp/NavHeader.js
@@ -17,12 +17,15 @@ export default function NavHeader () {
             <ul className={` max-md:bg-slate-500 max-md:h-screen max-md:w-[90vw] max-md:top-0 max-md:absolute max-md:ml-[10vw] max-md:z-10 max-md:duration-300 max-md:rounded-tl-[4rem] max-md:rounded-bl-[30rem]
                                 max-md:flex max-md:flex-col max-md:justify-center max-md:items-center
                                 ${hamburgerOpen?'':'translate-x-full duration-500'}s
-                                md:mr-4 md:flex md:flex-nowrap
+                                md:mr-24 md:flex md:flex-nowrap
                                 `}
                                 >
-                <li className="pr-4 pb-10 text-5xl">Home</li>
-                <li className="pr-4 pb-10 text-5xl">About</li>
-                <li className="pr-4 pb-10 mb-40 text-5xl">Contact US</li>
+                <li className="pr-4 max-md:pb-10 max-md:text-5xl
+                                md:text-lg md:font-semibold md:text-white">Home</li>
+                <li className="pr-4 max-md:pb-10 max-md:text-5xl
+                                md:text-lg md:font-semibold md:text-white">About</li>
+                <li className="pr-4 max-md:pb-10 max-md:mb-20 max-md:text-5xl
+                                md:text-lg md:font-semibold md:text-white">Contact US</li>
             </ul>
             <div className="z-20" onClick={toggleHamburger}>
                 <Hamburger isOpen={hamburgerOpen}/>

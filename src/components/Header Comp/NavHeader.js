78a6326086e5c6bcd35d32956355fa98f4a640f4
Pritam-00 <pritambera11235@gmail.com>
2023-12-20 08:25:13 +0530

    Mobile Menu UI Update

--- src/components/Header Comp/NavHeader.js
+++ src/components/Header Comp/NavHeader.js
@@ -14,18 +14,28 @@ export default function NavHeader () {
         <nav className="top-0 left-0 right-0 flex justify-between items-center
                         bg-gradient-to-b absolute from-black to-transparent h-20"> 
             <img className="p-4 h-24" src={icon} alt="The Trail Makers Logo"></img>
-            <ul className={` max-md:bg-slate-500 max-md:h-screen max-md:w-[90vw] max-md:top-0 max-md:absolute max-md:ml-[10vw] max-md:z-10 max-md:duration-300 max-md:rounded-tl-[4rem] max-md:rounded-bl-[30rem]
-                                max-md:flex max-md:flex-col max-md:justify-center max-md:items-center
-                                ${hamburgerOpen?'':'translate-x-full duration-500'}s
+            <ul className={`font-mono uppercase max-md:bg-neutral-950 max-md:text-white 
+                                max-md:absolute max-md:h-screen max-md:w-[70vw] max-md:ml-[30vw] max-md:top-0 
+                                max-md:z-10 max-md:duration-300 
+                                max-md:flex max-md:flex-col max-md:pt-28 max-md:items-end 
+                                ${hamburgerOpen?'':'translate-x-full duration-500'} 
                                 md:mr-24 md:flex md:flex-nowrap
                                 `}
                                 >
-                <li className="pr-4 max-md:pb-10 max-md:text-5xl
+                <li className="pr-4 max-md:pb-4 max-md:text-2xl
                                 md:text-lg md:font-semibold md:text-white">Home</li>
-                <li className="pr-4 max-md:pb-10 max-md:text-5xl
-                                md:text-lg md:font-semibold md:text-white">About</li>
-                <li className="pr-4 max-md:pb-10 max-md:mb-20 max-md:text-5xl
-                                md:text-lg md:font-semibold md:text-white">Contact US</li>
+                <li className="pr-4 max-md:pb-4 max-md:text-2xl
+                                md:text-lg md:font-semibold md:text-white">Expeditions</li>
+                <li className="pr-4 max-md:pb-4 max-md:text-2xl
+                                md:text-lg md:font-semibold md:text-white">Trekkings</li>
+                <li className="pr-4 max-md:pb-4 max-md:text-2xl
+                                md:text-lg md:font-semibold md:text-white">Courses</li>
+                <li className="pr-4 max-md:pb-4 max-md:text-2xl
+                                md:text-lg md:font-semibold md:text-white">Study Materials</li>
+                <li className="pr-4 max-md:pb-4 max-md:text-2xl
+                                md:text-lg md:font-semibold md:text-white">FAQs</li>
+                <li className="pr-4 max-md:pb-4 max-md:text-2xl
+                                md:text-lg md:font-semibold md:text-white">Enquire</li>
             </ul>
             <div className="z-20" onClick={toggleHamburger}>
                 <Hamburger isOpen={hamburgerOpen}/>

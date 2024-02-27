52a0f59cc35b2104e896bfa030ebdaf7aab1e9d5
Pritam-00 <pritambera11235@gmail.com>
2024-02-27 19:10:46 +0530

    whatsapp contact

--- src/components/CreateGroup/CreateGroup.js
+++ src/components/CreateGroup/CreateGroup.js
@@ -1,10 +1,19 @@
 import React from "react";
 
-export default function CreateGroup () {
-    return (
-        <div className="h-96 text-center m-40">
-            <h2>Hello, we haven't build the page yet</h2>
-            <h1>currently you can contact us on 7980426832, for any query and booking</h1>
-        </div>
-    )
+export default function CreateGroup() {
+  return (
+    <div>
+      <div className="h-96 text-center m-12 mt-24">
+        <h2 className="mb-4 font-semibold font-basierCircle">Hello, we haven't build the page properly yet</h2>
+        <h1 className="mb-4">
+          currently you will have to contact us on 7980426832(Whatsapp), for any
+          query and booking
+        </h1>
+        <a href="https://wa.me/<6289096041>">
+        <button className="p-4 rounded-2xl bg-black text-white">Ask any Query on Whatsapp</button>
+      </a>
+      </div>
+
+    </div>
+  );
 }
